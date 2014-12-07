fe7038fe1f981ca27842e7c89323d214a875a5ba
Dave Chmura <dschmura@humbledaisy.com>
2014-12-07 11:28:41 -0600

    added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title
+    base_title = "The blog of blogs"
+    @base_title = base_title
+    if @page_title.nil?
+      base_title
+    else
+      "#{@page_title}  | #{@base_title}"
+    end
+  end
 end
